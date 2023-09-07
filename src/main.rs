bb573207beb2f18891b9a9ea2dd2886560205ac6
Petr Kozelka <pkozelka@gmail.com>
2023-09-08 01:38:08 +0200

    default log level info

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use log::LevelFilter;
 fn main() -> anyhow::Result<()> {
     pretty_env_logger::formatted_timed_builder()
         .format_timestamp_millis()
-        .filter_level(LevelFilter::Warn)
+        .filter_level(LevelFilter::Info)
         .filter_module("serde_xml_rs", LevelFilter::Info)
         .filter_module("rustls", LevelFilter::Info)
         .filter_module("zip_extract", LevelFilter::Debug)
